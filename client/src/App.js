123368910bb12e7f1445ce3038a0fb9f0ae51b66
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2024-01-08 18:34:05 +0100

    aljdskn

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
+import React from "react";
+import { useState,useEffect } from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
+  const [data, setData] = React.useState([]);//Posts (too far in to rename
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, []);
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
+
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const path = require('path');
 const express = require("express");
 const { PrismaClient } = require('@prisma/client');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/',(req,res)=>{
+app.use(express.static(path.resolve(__dirname,'../client/build')));
+
+console.log('express listening')
+
+app.get('/api',(req,res)=>{
     res.json('hello, running')
 })
 
